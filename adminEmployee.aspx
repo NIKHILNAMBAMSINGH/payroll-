7c98bc038f1d627ab910d0ff1db22d84e795ce4a
Nikhil Nambam Singh <73900042+NIKHILNAMBAMSINGH@users.noreply.github.com>
2024-03-08 13:13:15 +0530

    Add files via upload

---
+++ adminEmployee.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage2.master" AutoEventWireup="true" CodeFile="adminEmployee.aspx.cs" Inherits="adminEmployee" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder2" Runat="Server">
+    <table class="auto-style2" style="width: 121%">
+        <tr>
+            <td><div id="heading">
+                  <asp:Label ID="lblEmpId" runat="server" Text="Employee ID:"></asp:Label>
+                    <asp:TextBox ID="txtEmpId" runat="server"></asp:TextBox>
+             
+                    <asp:Label ID="lblUserId" runat="server" Text="User ID:"></asp:Label>
+                    <asp:TextBox ID="txtUserId" runat="server"></asp:TextBox>
+              
+                    <asp:Label ID="lblFirstName" runat="server" Text="First Name:"></asp:Label>
+                    <asp:TextBox ID="txtFirstName" runat="server"></asp:TextBox>
+               <br />
+                    <asp:Label ID="lblLastName" runat="server" Text="Last Name:"></asp:Label>
+                    <asp:TextBox ID="txtLastName" runat="server"></asp:TextBox>
+
+                    <asp:Label ID="lblDob" runat="server" Text="Date of Birth:" ></asp:Label>
+                    <asp:TextBox ID="txtDob" runat="server"></asp:TextBox>       
+              
+                    <asp:Label ID="lblContactNumber" runat="server" Text="Contact Number:"></asp:Label>
+                    <asp:TextBox ID="txtContactNumber" runat="server"></asp:TextBox>
+                <br />
+                    <asp:Label ID="lblEmail" runat="server" Text="Email:"></asp:Label>
+                    <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+             <asp:Label ID="lblAddress" runat="server" Text="Address:"></asp:Label>
+         
+                   <asp:TextBox ID="txtAddress" runat="server"></asp:TextBox>
+              
+                   <asp:Label ID="lblDepartmentId" runat="server" Text="Department ID:"></asp:Label>
+                   <asp:TextBox ID="txtDepartmentId" runat="server"></asp:TextBox>
+                <br />
+           <asp:Label ID="lblGender" runat="server" Text="Gender:"></asp:Label>
+                    <asp:DropDownList ID="ddlGender" runat="server">
+                            <asp:ListItem Text="Male" Value="Male"></asp:ListItem>
+                            <asp:ListItem Text="Female" Value="Female"></asp:ListItem>
+                            <asp:ListItem Text="Other" Value="Other"></asp:ListItem>
+                        </asp:DropDownList>
+            
+                  <asp:Button ID="Button1" runat="server" OnClick="Button1_Click"  Text="Add Employee" />
+            
+            </div></td>
+        </tr>
+        <tr>
+            <td > <div id="GridViewContainer" >
+        <h2>Employee Details</h2>
+             <asp:GridView ID="GridView1" runat="server"  AutoGenerateColumns="False" AllowPaging="true"
+    OnPageIndexChanging="OnPageIndexChanging" PageSize="5" CellPadding="0"
+    OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" OnRowDeleting="GridView1_RowDeleting"
+    OnRowCancelingEdit="GridView1_RowCancelingEdit" DataKeyNames="EmpId" >
+   <Columns>
+    <asp:BoundField DataField="EmpId" HeaderText="Employee ID" ItemStyle-Width="100px" ReadOnly="true" />
+    <asp:BoundField DataField="UserId" HeaderText="User ID" ItemStyle-Width="100px"  ReadOnly="true" />
+    <asp:BoundField DataField="FirstName" HeaderText="First Name" ItemStyle-Width="150px" />
+    <asp:BoundField DataField="LastName" HeaderText="Last Name" ItemStyle-Width="150px" />
+    <asp:BoundField DataField="Dob" HeaderText="Date of Birth" ItemStyle-Width="100px" />
+    <asp:BoundField DataField="Gender" HeaderText="Gender" ItemStyle-Width="100px" />
+    <asp:BoundField DataField="ContactNumber" HeaderText="Contact Number" ItemStyle-Width="150px" />
+    <asp:BoundField DataField="Email" HeaderText="Email" ItemStyle-Width="200px" />
+    <asp:BoundField DataField="Address" HeaderText="Address" ItemStyle-Width="200px" />
+    <asp:BoundField DataField="DepartmentId" HeaderText="Department ID" ItemStyle-Width="100px" />
+    <asp:CommandField ShowEditButton="True" ShowDeleteButton="True" />
+</Columns>
+</asp:GridView>
+         </div></td>
+        </tr>
+    </table>
+</asp:Content>
+
---
+++ adminEmployee.aspx.cs
@@ -0,0 +1,202 @@
+﻿﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+public partial class adminEmployee : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            txtEmpId.Text = "";
+            txtUserId.Text = "";
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtDob.Text = "";
+            ddlGender.SelectedIndex = 0;
+            txtContactNumber.Text = "";
+            txtEmail.Text = "";
+            txtAddress.Text = "";
+            txtDepartmentId.Text = "";
+            BindGridView();
+        }
+    }
+
+    protected void btnResetEmployee_Click(object sender, EventArgs e)
+    {
+        ClearInputFields();
+    }
+
+    protected void BindGridView()
+    {
+        string _connectionString = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            string query = "SELECT * FROM dbo.EmployeeDetailslTbl";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string _connectionString = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            int empId = Convert.ToInt32(txtEmpId.Text);
+            int userId = Convert.ToInt32(txtUserId.Text);
+            string firstName = txtFirstName.Text;
+            string lastName = txtLastName.Text;
+            string dob = txtDob.Text;
+            string gender = ddlGender.SelectedValue;
+            string contactNumber = txtContactNumber.Text;
+            string email = txtEmail.Text;
+            string address = txtAddress.Text;
+            int departmentId = Convert.ToInt32(txtDepartmentId.Text);
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "INSERT INTO dbo.EmployeeDetailslTbl (empId, userId, FirstName, LastName, Dob, Gender, ContactNumber, Email, Address, DepartmentId) " +
+                               "VALUES (@EmpId, @UserId, @FirstName, @LastName, @Dob, @Gender, @ContactNumber, @Email, @Address, @DepartmentId)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@EmpId", empId);
+                command.Parameters.AddWithValue("@UserId", userId);
+                command.Parameters.AddWithValue("@FirstName", firstName);
+                command.Parameters.AddWithValue("@LastName", lastName);
+                command.Parameters.AddWithValue("@Dob", dob);
+                command.Parameters.AddWithValue("@Gender", gender);
+                command.Parameters.AddWithValue("@ContactNumber", contactNumber);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Address", address);
+                command.Parameters.AddWithValue("@DepartmentId", departmentId);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            BindGridView();
+
+            ClearInputFields();
+        }
+        catch (Exception ex)
+        {
+            // Handle exception
+        }
+    }
+
+    protected void OnPageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        GridView1.PageIndex = e.NewPageIndex;
+        BindGridView();
+    }
+
+    protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        GridView1.EditIndex = e.NewEditIndex;
+        BindGridView();
+    }
+
+    protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        int rowIndex = e.RowIndex;
+        GridViewRow row = GridView1.Rows[rowIndex];
+
+        int empId = Convert.ToInt32(GridView1.DataKeys[rowIndex].Values["empId"]);
+        int newEmpId = Convert.ToInt32(((TextBox)row.Cells[0].Controls[0]).Text); // Assuming the empId textbox is in the first column
+        int userId = Convert.ToInt32(((TextBox)row.Cells[1].Controls[0]).Text);
+        string firstName = ((TextBox)row.Cells[2].Controls[0]).Text;
+        string lastName = ((TextBox)row.Cells[3].Controls[0]).Text;
+        string dob = ((TextBox)row.Cells[4].Controls[0]).Text;
+        string gender = ((DropDownList)row.Cells[5].Controls[1]).SelectedValue;
+        string contactNumber = ((TextBox)row.Cells[6].Controls[0]).Text;
+        string email = ((TextBox)row.Cells[7].Controls[0]).Text;
+        string address = ((TextBox)row.Cells[8].Controls[0]).Text;
+        int departmentId = Convert.ToInt32(((TextBox)row.Cells[9].Controls[0]).Text);
+
+        try
+        {
+            string _connectionString = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "UPDATE dbo.EmployeeDetailslTbl SET empId = @NewEmpId, userId = @UserId, FirstName = @FirstName, " +
+                               "LastName = @LastName, Dob = @Dob, Gender = @Gender, ContactNumber = @ContactNumber, Email = @Email, " +
+                               "Address = @Address, DepartmentId = @DepartmentId WHERE empId = @EmpId";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@EmpId", empId);
+                command.Parameters.AddWithValue("@NewEmpId", newEmpId);
+                command.Parameters.AddWithValue("@UserId", userId);
+                command.Parameters.AddWithValue("@FirstName", firstName);
+                command.Parameters.AddWithValue("@LastName", lastName);
+                command.Parameters.AddWithValue("@Dob", dob);
+                command.Parameters.AddWithValue("@Gender", gender);
+                command.Parameters.AddWithValue("@ContactNumber", contactNumber);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Address", address);
+                command.Parameters.AddWithValue("@DepartmentId", departmentId);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            GridView1.EditIndex = -1;
+            BindGridView();
+        }
+        catch (Exception ex)
+        {
+            // Handle exception
+        }
+    }
+
+    protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        int rowIndex = e.RowIndex;
+        int empId = Convert.ToInt32(GridView1.DataKeys[rowIndex].Values["empId"]);
+
+        try
+        {
+            string _connectionString = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "DELETE FROM dbo.EmployeeDetailslTbl WHERE empId=@EmpId";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@EmpId", empId);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+            BindGridView();
+        }
+        catch (Exception ex)
+        {
+            // Handle exception
+        }
+    }
+
+    protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        GridView1.EditIndex = -1;
+        BindGridView();
+    }
+
+    private void ClearInputFields()
+    {
+        txtEmpId.Text = "";
+        txtUserId.Text = "";
+        txtFirstName.Text = "";
+        txtLastName.Text = "";
+        txtDob.Text = "";
+        ddlGender.SelectedIndex = 0;
+        txtContactNumber.Text = "";
+        txtEmail.Text = "";
+        txtAddress.Text = "";
+        txtDepartmentId.Text = "";
+    }
+}
